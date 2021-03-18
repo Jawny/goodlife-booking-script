bdb70c03890a0ac93428baf9fc92b64a4dea1809
Johnny Liao <johnnyliao06@gmail.com>
2021-03-17 23:02:47 -0700

    updated defaults for test file

--- test/verifyLogin.test.js
+++ test/verifyLogin.test.js
@@ -1,6 +1,6 @@
 const { verifyLoginCredentials } = require("../utils/index.js");
-const EMAIL = "johnnyliao06@gmail.com";
-const PASSWORD = "Goodlifeautobook123!";
+const EMAIL = "";
+const PASSWORD = "";
 
 const testLogin = async () => {
   const loginResponse = await verifyLoginCredentials(EMAIL, PASSWORD);
